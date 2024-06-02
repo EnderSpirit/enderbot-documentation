1bdb08d8229ca61581145558ec46289d788fd4de
EnderSpirit <28017013+EnderSpirit@users.noreply.github.com>
2024-06-02 12:54:45 +0200

    Fixed TypeScript tsconfig React JSX linting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
-
 import styles from './index.module.css';
+import React from 'react';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
